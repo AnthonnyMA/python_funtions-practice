ae33c24a4ef5a37f72649083f370097ef73604d6
michaelllu <michaellu071205@gmail.com>
2026-05-05 15:24:35 -0600

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,8 +1,8 @@
 def summation(n):
     return sum(range(1, n+1))
 
-def summation(n):
-    summation = 0
+def summation2(n):
+    summation2 = 0
     for i in range(n + 1):
-        summation += i
-    return summatin
+        summation2 += i
+    return summation2
